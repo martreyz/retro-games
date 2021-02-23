8fe4a2f8a94ae040233e249fa68f85d08a7e0a6e
martreyz <martreyz@gmail.com>
2021-02-23 18:52:41 +0100

    Add ghosts movement functionalities

--- js/pacman.js
+++ js/pacman.js
@@ -21,9 +21,12 @@ const board = document.querySelector(".board");
 
 let squares = [];
 let score = 0;
+let level = 1;
 
 const scoreContainer = document.querySelector(".score");
 scoreContainer.innerHTML = score;
+const levelContainer = document.querySelector(".level");
+levelContainer.innerHTML = level;
 
 const completeBoard = () => {
   for (let i = 0; i < boardStyles.length; i++) {
@@ -48,6 +51,7 @@ const completeBoard = () => {
     }
   }
   startPoint();
+  ghostPlacement();
 };
 
 //Pacman movement:
@@ -56,11 +60,13 @@ const completeBoard = () => {
 let pacmanStart = 490;
 const pacman = document.createElement("div");
 
+//Start placement pacman:
 const startPoint = () => {
   pacman.classList.add("gamePacman");
   squares[pacmanStart].appendChild(pacman);
 };
 
+//Movement and feed pacman:
 const pacmanMoving = (ev) => {
   squares[pacmanStart].removeChild(pacman);
   switch (ev.keyCode) {
@@ -92,9 +98,11 @@ const pacmanMoving = (ev) => {
         if (pacmanStart === 393) {
           squares[419].innerHTML = "";
           pacmanStart = 419;
+          pacman.style.transform = "rotate(180deg)";
         } else {
           squares[pacmanStart - 1].innerHTML = "";
           pacmanStart -= 1;
+          pacman.style.transform = "rotate(180deg)";
         }
       }
       break;
@@ -125,6 +133,7 @@ const pacmanMoving = (ev) => {
         }
         squares[pacmanStart - 28].innerHTML = "";
         pacmanStart -= 28;
+        pacman.style.transform = "rotate(270deg)";
       }
       break;
     case 39:
@@ -155,9 +164,11 @@ const pacmanMoving = (ev) => {
         if (pacmanStart === 418) {
           squares[393].innerHTML = "";
           pacmanStart = 392;
+          pacman.style.transform = "rotate(0deg)";
         } else {
           squares[pacmanStart + 1].innerHTML = "";
           pacmanStart += 1;
+          pacman.style.transform = "rotate(0deg)";
         }
       }
       break;
@@ -188,10 +199,12 @@ const pacmanMoving = (ev) => {
         }
         squares[pacmanStart + 28].innerHTML = "";
         pacmanStart += 28;
+        pacman.style.transform = "rotate(90deg)";
       }
       break;
   }
   squares[pacmanStart].appendChild(pacman);
+  checkLevel();
 };
 
 const removeClass = () => {
@@ -199,3 +212,116 @@ const removeClass = () => {
 };
 
 document.addEventListener("keyup", pacmanMoving);
+
+//Level up:
+
+const checkLevel = () => {
+  if (score < 50) {
+    level = 1;
+    levelContainer.innerHTML = level;
+  } else if (score >= 50 && score < 100) {
+    level = 2;
+    levelContainer.innerHTML = level;
+  } else if (score >= 100 && score < 150) {
+    level = 3;
+    levelContainer.innerHTML = level;
+  } else if (score >= 150 && score < 250) {
+    level = 4;
+    levelContainer.innerHTML = level;
+  } else if (score >= 250 && score < 350) {
+    level = 5;
+    levelContainer.innerHTML = level;
+  } else if (score >= 350 && score < 450) {
+    level = 6;
+    levelContainer.innerHTML = level;
+  } else if (score >= 450 && score < 550) {
+    level = 7;
+    levelContainer.innerHTML = level;
+  } else if (score >= 550 && score < 650) {
+    level = 8;
+    levelContainer.innerHTML = level;
+  } else if (score >= 650 && score < 700) {
+    level = 9;
+    levelContainer.innerHTML = level;
+  } else if (score >= 700) {
+    level = 10;
+    levelContainer.innerHTML = level;
+  }
+};
+
+//Ghosts:
+
+let ghosts = [
+  document.createElement("div"),
+  document.createElement("div"),
+  document.createElement("div"),
+  document.createElement("div"),
+  document.createElement("div"),
+  document.createElement("div"),
+];
+
+let startPoints = [403, 404, 405, 406, 407, 408];
+let classes = [
+  "ghostOne",
+  "ghostTwo",
+  "ghostThree",
+  "ghostOne",
+  "ghostTwo",
+  "ghostThree",
+];
+
+//Place ghosts in the board:
+
+const ghostPlacement = () => {
+  for (let i = 0; i < ghosts.length; i++) {
+    ghosts[i].classList.add(classes[i]);
+    squares[startPoints[i]].appendChild(ghosts[i]);
+  }
+};
+
+let previousDirection = [-28, -28, -28, -28, -28, -28];
+let preferredDirection = [1, -1, 28, -28, 1, -1];
+//Ghosts movement:
+const ghostMovement = () => {
+  let directions = [1, -1, 28, -28];
+  for (let i = 0; i < ghosts.length; i++) {
+    let randomDirection = Math.round(Math.random() * 1);
+    if (
+      previousDirection[i] !== 28 &&
+      !squares[startPoints[i] - 28].classList.contains("One") &&
+      squares[startPoints[i]].classList.contains("Five") &&
+      startPoints.indexOf(startPoints[i] - 28) === -1
+    ) {
+      startPoints[i] -= 28;
+    } else if (
+      previousDirection[i] &&
+      !squares[startPoints[i] + previousDirection[i]].classList.contains(
+        "One"
+      ) &&
+      startPoints.indexOf(startPoints[i] + previousDirection[i]) === -1
+    ) {
+      startPoints[i] += previousDirection[i];
+    } else {
+      if (previousDirection[i] === 1 || previousDirection[i] === -1) {
+        directions = [28, -28];
+      } else {
+        directions = [1, -1];
+      }
+      if (
+        !squares[
+          startPoints[i] + directions[randomDirection]
+        ].classList.contains("One") &&
+        startPoints.indexOf(startPoints[i] + directions[randomDirection]) === -1
+      ) {
+        startPoints[i] += directions[randomDirection];
+        previousDirection[i] = directions[randomDirection];
+      }
+    }
+  }
+
+  ghostPlacement();
+};
+
+let ms = 200 - level * 50;
+
+setInterval(ghostMovement, ms);
